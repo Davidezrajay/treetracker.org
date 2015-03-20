3880bb1dbd7cdfc0fad5977832b8c9254a3f286d
Kresimir Plese <kresimir.plese@gmail.com>
2015-03-20 14:54:14 -0400

    Database pass changed.

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'treetracker',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'fQNQJPpFHG',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
