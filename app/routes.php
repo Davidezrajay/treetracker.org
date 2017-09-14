28aa931c3f97077d18db74b42dfb1e2afc1309d0
Matthew Xi <matt@winterroot.net>
2017-09-13 18:13:14 -0700

    Cleaning up routes

--- app/routes.php
+++ app/routes.php
@@ -11,56 +11,17 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-Route::get('/', 'HomeController@showWelcome');
-Route::get('/showtreedata/{id}', 'HomeController@showTreeData');
 
+// Website
+Route::get('/', 'MapController@showWelcome');
+Route::get('/showtreedata/{id}', 'MapController@showTreeData');
+
+
+// API
 Route::resource('users/signup', 'UserController@signup');
 Route::resource('users/login', 'UserController@login');
 Route::controller('password', 'RemindersController');
 
-
-//Route::group(array('before' => 'token'), function()
-//{
-	
-	Route::resource('users', 'UserController');
-	
-	Route::get('trees/show', 'TreeController@showTrees');
-	
-	// Route::get('trees/show', array('before' => 'token', 'uses' => 'TreeController@showTrees'));
-	
-	
-	Route::resource('trees', 'TreeController');
-	Route::resource('settings', 'SettingController');
-	Route::resource('trees/updates', 'TreeUpdatesController');
-	//Route::resource('trees/updates/user', 'TreeUpdatesController@userPendingUpdates');
-    Route::resource('trees/updates/user', 'TreeUpdatesController@testPendingUpdate');
-    Route::resource('trees/updates/test/user', 'TreeUpdatesController@testPendingUpdate');
-	Route::resource('trees/user', 'TreeController@getUserTrees');
-    Route::resource('trees/json/all', 'TreeController@getTreesJson');
-
-
-// 	Route::delete('trees/updates/clear', 'TreeUpdatesController@clearPendingUpdates');
-//});
-
-
-// Route::resource('users', 'UserController');
-// Route::resource('users/signup', 'UserController@signup');
-// Route::resource('users/login', 'UserController@login');
-
-
-// Route::resource('users/reset', 'RemindersController@postRemind');
-
-// Route::controller('password', 'RemindersController');
-
-// Route::get('trees/show', 'TreeController@showTrees');
-
-// // Route::get('trees/show', array('before' => 'token', 'uses' => 'TreeController@showTrees'));
-
-
-// Route::resource('trees', 'TreeController');
-// Route::resource('trees/updates', 'TreeUpdatesController');
-// Route::resource('trees/updates/user', 'TreeUpdatesController@userPendingUpdates');
-// Route::delete('trees/updates/clear', 'TreeUpdatesController@clearPendingUpdates');
 Route::resource('trees/details/user', 'TreeController2@getUserTreesDetailed');
 Route::resource('trees2/create', 'TreeController2@create');
 Route::resource('trees2/update', 'TreeController2@updateTree');
